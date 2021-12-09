5f47fe418f176a7187861118895ac0073143a0c1
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2021-12-09 13:32:31 +0200

    Added 'DB Operations' module and implemented GET request to JSON database

--- src/app/main/main-page/main-page.component.ts
+++ src/app/main/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Author } from 'src/app/interfaces/author';
+import { DbOperationsService } from 'src/app/services/db-operations.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  //bookItem : HTMLElement | undefined;
+  //authors? : [string, string, string, Date, string[], string[]];
+  authors : any = [];
+
+  constructor(private dbOperationsService : DbOperationsService) { }
+
+  getAuthors() {
+    this.dbOperationsService.getAuthors().subscribe(item => {
+      console.log(item);
+      this.authors = <Author>item;
+    })
+  }
 
   ngOnInit(): void {
+    this.getAuthors();
   }
 
 }

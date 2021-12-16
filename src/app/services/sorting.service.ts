1d48bce547275fc3e3d7dc74521b47b9bd7d28b9
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2021-12-16 23:32:05 +0200

    Implemented highlightning for only chosen column

--- src/app/services/sorting.service.ts
+++ src/app/services/sorting.service.ts
@@ -8,7 +8,12 @@ export class SortingService {
 
   constructor() { }
 
+  arrayOfHighlightenedColumns : any = [];
+
   sort(authorsData : Author[], sortKey: string) {
+
+    //let count = this.clojureFunction();
+    //let fucn = this.funcTest(authorsData, sortKey as keyof Author);
 
     switch (sortKey) {
 
@@ -16,6 +21,11 @@ export class SortingService {
       case 'surname': 
       case 'patronymic': {
         this.sortAuthorsBy_Name_Surname_Patronymic(authorsData, sortKey);
+        this.highlightSortedKeyColumns(authorsData, sortKey);
+        // console.log(count());
+        // console.log(count());
+        //console.log(fucn());
+        //console.log(fucn());
         break;
       }
       case 'birthDate': {
@@ -31,17 +41,71 @@ export class SortingService {
 
   sortAuthorsBy_Name_Surname_Patronymic(data : Author[], key : keyof Author) {
     return data.sort(function(firstObject, secondObject) {
-      if (firstObject[key] < secondObject[key]) {
-        return -1;
-      }
-      else if (firstObject[key] > secondObject[key]) {
-        return 1;
-      }
-      else {
-        return 0;
-      }
+
+      return firstObject[key] > secondObject[key] ? 1 : firstObject[key] < secondObject[key] ? -1 : 0;
+
+      // if (firstObject[key] < secondObject[key]) return -1;
+      // else if (firstObject[key] > secondObject[key]) return 1;
+      // else return 0;
+
     });
   }
+
+  highlightSortedKeyColumns(data : Author[], key : keyof Author) : NodeListOf<Element> {
+
+    const sortedDataColumns = document.querySelectorAll('.' + key);
+
+    this.arrayOfHighlightenedColumns.push(sortedDataColumns);
+
+    if (this.arrayOfHighlightenedColumns.length > 1) {
+    
+      for (let el in this.arrayOfHighlightenedColumns) {
+
+        for (let el of this.arrayOfHighlightenedColumns[0]) {
+          el.removeAttribute("style");
+        }
+        this.arrayOfHighlightenedColumns.splice(0, el);
+      }
+      
+    }
+
+    for (let element of sortedDataColumns) {
+      element.setAttribute("style", "background-color: lightgreen;");
+    }
+
+    console.log(this.arrayOfHighlightenedColumns);
+    return sortedDataColumns;
+
+  }
+
+  // clojureFunction() {
+
+  //     let current = 0;
+  //     return function () {
+  //       current++;
+  //       return current;
+  //     }
+    
+  // };
+
+  // funcTest(data: Author[], key : keyof Author) {
+
+  //   let arr : any = [];
+  //   let sortedDataColumns = document.querySelectorAll('.' + key);
+
+  //   return function() {
+
+  //     for (let element of sortedDataColumns) {
+  //       arr.push(element.setAttribute("style", "background-color: lightgreen;"));
+  //     }
+
+  //     for (let arrEl of arr) {
+  //       console.dir(arrEl);
+  //     }
+
+  //     return arr;
+  //   }
+  // }
 
   sortAuthorsByBirth() {
 

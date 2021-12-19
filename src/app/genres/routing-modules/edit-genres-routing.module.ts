32ab3c495b19baa8af08ded34b64176c9641a05d
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2021-12-20 01:18:38 +0200

    Restored data of unsuccessful commit. Designing EditGenres, basic configuration of component, added GET and DELETE methods

--- src/app/genres/routing-modules/edit-genres-routing.module.ts
+++ src/app/genres/routing-modules/edit-genres-routing.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GenresComponent } from '../genres.component';
@@ -7,7 +8,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  declarations: [GenresComponent],         //--------------------Genres COmponent should be added
+  imports: [CommonModule, RouterModule.forChild(routes)], //-----------COmmonModule should be added (*ngFor problem)
   exports: [RouterModule]
 })
 export class EditGenresRoutingModule { }

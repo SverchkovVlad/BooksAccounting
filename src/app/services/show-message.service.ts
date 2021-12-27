05bff4b80391b0c73d05f518076634f12a91d873
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2021-12-27 12:46:41 +0200

    Added ShowMessage service. Moved ShowMessage methods from genre.ts to ShowMessage service

---
+++ src/app/services/show-message.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShowMessageService {
+
+  constructor() { }
+
+  showInfo(cssClass: string, inputText: string, typeOfMessage: string) {
+    let element = document.querySelector('.blank') as HTMLElement;
+
+    if (element.classList.item(1)) element.classList.remove(element.classList.item(1) as string);
+
+    element.classList.add(cssClass);
+
+    switch (typeOfMessage) {
+      case 'add': 
+        element.innerHTML = "You have successfully added genre <b>" + inputText + "</b>";
+        break;
+      case 'edit': 
+        element.innerHTML = "New name of selected genre is <b>" + inputText + "</b>";
+        break;
+      case 'error-empty':
+        element.innerHTML = "Input field cannot be empty!";
+        break;
+    }
+    
+    element.style.display = "block";
+
+    window.setTimeout(this.hideElement, 2500, element);
+  }
+
+  hideElement(element : HTMLDivElement) {
+    element.style.display = "none";
+  }
+
+}

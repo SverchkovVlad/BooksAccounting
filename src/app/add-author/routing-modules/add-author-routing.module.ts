4de759a6c180cb05dc0ba9ce0da40cdcd933b165
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2021-12-28 23:39:42 +0200

    Finished design of AddAuthor Page

--- src/app/add-author/routing-modules/add-author-routing.module.ts
+++ src/app/add-author/routing-modules/add-author-routing.module.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddAuthorComponent } from '../add-author.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', component: AddAuthorComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  declarations: [AddAuthorComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AddAuthorRoutingModule { }

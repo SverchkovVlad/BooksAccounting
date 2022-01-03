3a8f4d6a8c8f3e39781f9a46cdbb3f276d37d956
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2022-01-03 21:34:05 +0200

    First workable version of adding Books to Author

--- src/app/add-author/add-author.component.ts
+++ src/app/add-author/add-author.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Genre } from '../interfaces/genre';
 import { DbOperationsService } from '../services/db-operations.service';
 
@@ -11,11 +11,13 @@ import { DbOperationsService } from '../services/db-operations.service';
 export class AddAuthorComponent implements OnInit {
 
   formAddAuthor: FormGroup = new FormGroup({});
+  books: FormArray = new FormArray([]);
   genres: Genre[];
+  genreValue: string;
 
   constructor(
     private dbOperationService: DbOperationsService,
-    private fBuilder : FormBuilder) { }
+    private fb : FormBuilder) { }
 
   getGenres() {
     this.dbOperationService.getGenres().subscribe(genre => {
@@ -24,14 +26,36 @@ export class AddAuthorComponent implements OnInit {
   }
 
   createFormElements() {
-    this.formAddAuthor = this.fBuilder.group({
-      name : this.fBuilder.control('', [Validators.pattern('^[a-zA-Z]+$'), Validators.required]),
-      surname : this.fBuilder.control('', [Validators.pattern('^[a-zA-Z]+$'), Validators.required]),
-      patronymic : this.fBuilder.control('', [Validators.pattern('^[a-zA-Z]+$'), Validators.required]),
-      dateOfBirth : this.fBuilder.control('', [Validators.required, 
-        Validators.pattern(/^(([0][1-9])|([1-2][0-9])|([3][0-1]))\/(([0][1-9])|([1][0-2]))\/([0-9]{4})$/)])
+    this.formAddAuthor = this.fb.group({
+      name : this.fb.control('', [Validators.pattern('^[a-zA-Z]+$'), Validators.required]),
+      surname : this.fb.control('', [Validators.pattern('^[a-zA-Z]+$'), Validators.required]),
+      patronymic : this.fb.control('', [Validators.pattern('^[a-zA-Z]+$'), Validators.required]),
+      dateOfBirth : this.fb.control('', [Validators.required, 
+        Validators.pattern(/^(([0][1-9])|([1-2][0-9])|([3][0-1]))\/(([0][1-9])|([1][0-2]))\/([0-9]{4})$/)]),
         // Input format: 16/03/1965
+      books: this.fb.array([this.createBook()])
     });
+  }
+
+  createBook() : FormGroup {
+    return this.fb.group({
+      bookName: this.fb.control('', [Validators.required]),
+      bookPagesNum: this.fb.control('', 
+      [Validators.required, Validators.min(1), Validators.pattern(/^[0-9]\d*$/)]),
+      bookGenre: this.fb.control(this.genreValue, Validators.required)
+    });
+  }
+
+  // createBook() {
+  //   return new FormGroup({
+  //     bookName: new FormControl(),
+  //     bookPagesNum: new FormControl()
+  //   })
+  // }
+
+  addBook() {
+    this.books = this.formAddAuthor.get('books') as FormArray;
+    this.books.push(this.createBook());
   }
 
   ngOnInit(): void {

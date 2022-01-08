d082741e615a230e38b772c44dafc60ae17d4637
Sverchkov Vladyslav <sverchkov.vladyslav@gmail.com>
2022-01-08 23:49:09 +0200

    Fixed bookPageNum sending as number data type. Added SuccessMessageDiv after author adding

--- src/app/add-author/add-author.component.ts
+++ src/app/add-author/add-author.component.ts
@@ -3,6 +3,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 import { Author } from '../interfaces/author';
 import { Genre } from '../interfaces/genre';
 import { DbOperationsService } from '../services/db-operations.service';
+import { ShowMessageService } from '../services/show-message.service';
 
 @Component({
   selector: 'app-add-author',
@@ -15,11 +16,12 @@ export class AddAuthorComponent implements OnInit {
   books: FormArray = new FormArray([]);
   genres: Genre[];
   genreValue: string;
-  authors : Author[];
+  authors: Author[];
 
   constructor(
     private dbOperationService: DbOperationsService,
-    private fb : FormBuilder) { }
+    private fb: FormBuilder,
+    private showMessageService: ShowMessageService) { }
 
   getGenres() {
     this.dbOperationService.getGenres().subscribe(genre => {
@@ -30,22 +32,22 @@ export class AddAuthorComponent implements OnInit {
   createFormElements() {
     //original regexp: ^[a-zA-Z,.-` ]+$ - problems with numbers
     this.formAddAuthor = this.fb.group({
-      name : this.fb.control('', [Validators.pattern('[^ ][a-zA-Z,.` -]+$'), Validators.required]),
-      surname : this.fb.control('', [Validators.pattern('[^ ][a-zA-Z,.` -]+$'), Validators.required]),
-      patronymic : this.fb.control('', [Validators.pattern('[^ ][a-zA-Z,.` -]+$'), Validators.required]),
-      birthDate : this.fb.control('', [Validators.required, 
-        Validators.pattern(/^(([0][1-9])|([1-2][0-9])|([3][0-1]))\/(([0][1-9])|([1][0-2]))\/([0-9]{4})$/)]),
-        // Input format: 16/03/1965
+      name: this.fb.control('', [Validators.pattern('[^ ][a-zA-Z,.` -]+$'), Validators.required]),
+      surname: this.fb.control('', [Validators.pattern('[^ ][a-zA-Z,.` -]+$'), Validators.required]),
+      patronymic: this.fb.control('', [Validators.pattern('[^ ][a-zA-Z,.` -]+$'), Validators.required]),
+      birthDate: this.fb.control('', [Validators.required,
+      Validators.pattern(/^(([0][1-9])|([1-2][0-9])|([3][0-1]))\/(([0][1-9])|([1][0-2]))\/([0-9]{4})$/)]),
+      // Input format: 16/03/1965
       books: this.fb.array([this.createBook()])
     });
   }
 
-  createBook() : FormGroup {
+  createBook(): FormGroup {
     return this.fb.group({
       //Validators.pattern('^[-a-zA-Z,.`?&*%#()<> ]+$')
       bookName: this.fb.control('', [Validators.pattern('[^ ](.*)'), Validators.required]), //not allowed first char as whitespace, but then - everything you want 
-      bookPagesNum: this.fb.control(1, 
-      [Validators.required, Validators.min(1), Validators.pattern(/^[0-9]\d*$/)]),
+      bookPagesNum: this.fb.control(<number>1,
+        [Validators.required, Validators.min(1), Validators.pattern(/^[0-9]\d*$/)]),
       bookGenre: this.fb.control(this.genreValue, Validators.required)
     });
   }
@@ -59,14 +61,27 @@ export class AddAuthorComponent implements OnInit {
     this.books.removeAt(this.books.length - 1);
   }
 
-  submitData() {
-    //let data = JSON.stringify(this.formAddAuthor.value);
-    let data = this.formAddAuthor.value;
-    data.bookPagesNum = +this.formAddAuthor.value.bookPagesNum; // converting numPages to number
+  submitData(form: FormGroup) {
 
-    this.dbOperationService.setAuthor(data).subscribe(() => {
-      this.authors.push(data);
-    })
+    if (form.valid && form.status == "VALID") {
+
+      let authorsData = form.value;
+
+      for (let element of authorsData.books) {
+        element.bookPagesNum = +element.bookPagesNum; // converting numPages to number
+      }
+
+      this.dbOperationService.setAuthor(authorsData).subscribe(() => {
+        this.authors.push(authorsData);
+      });
+
+      this.showMessageService.showInfo('success-add-author', '', '');
+
+    }
+
+    else {
+      alert("Error!");
+    }
   }
 
   ngOnInit(): void {
